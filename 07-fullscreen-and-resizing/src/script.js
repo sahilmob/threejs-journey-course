5b2f53426d8bab3837d064cc0b8620c7c4a6d479
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-11-30 11:39:56 +0300

    update camera and renderer

--- 07-fullscreen-and-resizing/src/script.js
+++ 07-fullscreen-and-resizing/src/script.js
@@ -26,6 +26,15 @@ const sizes = {
   height: window.innerHeight,
 };
 
+window.addEventListener("resize", (e) => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+  renderer.setSize(sizes.width, sizes.height);
+});
+
 /**
  * Camera
  */
