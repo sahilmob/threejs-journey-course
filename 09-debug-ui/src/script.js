845940549beb18602a418998a961b48f36f15452
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-11-30 13:54:03 +0300

    add elevation tweaking

--- 09-debug-ui/src/script.js
+++ 09-debug-ui/src/script.js
@@ -21,7 +21,7 @@ const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
 const material = new THREE.MeshBasicMaterial({ color: "#ff0000" });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
-gui.add(mesh.position, "y", -3, 3, 0.01);
+gui.add(mesh.position, "y").min(-3).max(3).step(0.01).name("elevation");
 
 /**
  * Sizes
