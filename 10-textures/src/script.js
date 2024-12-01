4b15a599c93e9ff912d989055f6c07b1691351da
Sahil H. Mobaidin <sahil.hmob@hotmail.com>
2024-12-01 13:02:08 +0300

    use loading manager and different types of textures

--- 10-textures/src/script.js
+++ 10-textures/src/script.js
@@ -9,8 +9,15 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 // };
 // image.src = "/textures/door/color.jpg";
 
-const textureLoader = new THREE.TextureLoader();
-const texture = textureLoader.load(
+const loadingManager = new THREE.LoadingManager();
+
+loadingManager.onStart = () => {};
+loadingManager.onError = () => {};
+loadingManager.onProgress = () => {};
+loadingManager.onLoad = () => {};
+
+const textureLoader = new THREE.TextureLoader(loadingManager);
+const colorTexture = textureLoader.load(
   "/textures/door/color.jpg"
   //   () => {
   //     // load
@@ -22,7 +29,16 @@ const texture = textureLoader.load(
   //     // error
   //   }
 );
-texture.colorSpace = THREE.SRGBColorSpace;
+const alphaTexture = textureLoader.load("/textures/door/alpha.jpg");
+const heightTexture = textureLoader.load("/textures/door/height.jpg");
+const normalTexture = textureLoader.load("/textures/door/normal.jpg");
+const ambientOcclusionTexture = textureLoader.load(
+  "/textures/door/ambientOcclusion.jpg"
+);
+const metalnessTexture = textureLoader.load("/textures/door/metalness.jpg");
+const roughnessTexture = textureLoader.load("/textures/door/roughness.jpg");
+
+colorTexture.colorSpace = THREE.SRGBColorSpace;
 
 /**
  * Base
@@ -37,7 +53,7 @@ const scene = new THREE.Scene();
  * Object
  */
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ map: texture });
+const material = new THREE.MeshBasicMaterial({ map: colorTexture });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
